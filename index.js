868f58dd874750da56459e59d14631cdd232da34
Ambika Kumari <30942672+Ambika4@users.noreply.github.com>
2020-06-01 10:51:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const port = 8000;
+const fs = require('fs');
+
+function requestHandler(req,res)
+{
+    console.log(req.url);
+
+    res.writeHead(200,{'content-type':'text/html'});
+
+    let filePath;
+
+    switch (req.url)
+    {
+        case '/':
+            filePath='./index.html'
+            break;
+        case '/resume':
+            filePath='./resume.html'
+            break;
+        
+        case '/thankyou':
+            filePath='./thankyou.html'
+            break;
+
+        default :
+            filepath='./404.html'
+
+    }
+
+    fs.readFile(filePath,function(err,data)
+    {
+        if(err)
+        {
+            console.log('Error!!',err);
+            return res.end('<h1>Error!!!</h1>');
+        }
+
+        return res.end(data);
+
+    });
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, function(err)
+{
+if(err){
+      console.log(err);
+      return;
+     
+      }
+    console.log("server is up and running on port:",port);
+
+});
